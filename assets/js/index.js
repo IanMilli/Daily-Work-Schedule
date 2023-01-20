5cdd9374d179057d224882e0f8edc5e21b0441df
Ian M <ianmillichamp.2@gmail.com>
2023-01-20 15:55:37 +0000

    made a global variable called currentDateTime which is equal to the jquery class, container referring to the h2 class in the html file

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+/*write the js to make a day work scheduler operate dynamically with css and html*/
+
+/*define global variables in this section*/
+let container = $(".container");
+let currentDayTime =$("<h2>");
+
+
+/*define todays date and time and print to the top of the page in h2 class 'currentDayTime' */
+
+
+
