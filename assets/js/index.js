c048c349b0a67c4eb3802ae47e32200488b83abc
Ian M <ianmillichamp.2@gmail.com>
2023-01-20 16:08:00 +0000

    realised jquery connection files still not connecting properly, carried out research on stack flow and reset script links in html file - current day, date and time now printing to webpage - bug resolved

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,13 @@
 
 /*define global variables in this section*/
 let container = $(".container");
-let currentDayTime =$("<h2>");
+let currentDayTime = $(".currentDayTime")
+let currentDayTimeText =$("<h2>");
 
 
 /*define todays date and time and print to the top of the page in h2 class 'currentDayTime' */
 
+currentDayTimeText.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+currentDayTime.append(currentDayTimeText);
 
 
