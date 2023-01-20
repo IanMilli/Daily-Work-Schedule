b8830c79dcd8b8002c1b1654fd9b5ea7098033dc
Ian M <ianmillichamp.2@gmail.com>
2023-01-20 16:20:52 +0000

    added function to display current date and time when page is accessed to assist user

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,20 @@
 
 /*define global variables in this section*/
 let container = $(".container");
-let currentDayTime = $(".currentDayTime")
-let currentDayTimeText =$("<h2>");
+let currentDayTime = $(".currentDayTime");
+let currentDayTimeText = $("<h2>");
 
 
 /*define todays date and time and print to the top of the page in h2 class 'currentDayTime' */
-
-currentDayTimeText.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+/*currentDayTimeText.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 currentDayTime.append(currentDayTimeText);
 
+the above tested and works but creates a static time display - i want a dynamic one so need to set in a function to advance the time by one second
+for every second on the clock*/
 
+setInterval(
+    function () {
+        currentDayTimeText.text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+        currentDayTime.append(currentDayTimeText);
+
+    }, 1000);
