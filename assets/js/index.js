5eab21f357b2fc7475632cc669e6c1c49cf08191
Ian M <ianmillichamp.2@gmail.com>
2023-01-20 19:07:45 +0000

    added js for loop to create day planner, appears correct if missing one or two details

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ let container = $(".container");
 let currentDayTime = $(".currentDayTime");
 let currentDayTimeText = $("<h2>");
 let currentTimePeriod = moment().format("hA");
-
+let timeColor = "";
 /*create required arrays here */
 
 let timeSlot = ["07.00", "08.00", "09.00", "10.00", "11.00", "12.00", "13.00", "14.00", "15.00", "16.00", "17.00"]
@@ -37,17 +37,18 @@ function displayCurrentTime() {
 then create a variable that is equal to the current hour so we can compare it against the array to generate the correct display color*/
     for (i = 0; i <= timeSlot.length; i++) {
 /*first iterate along the whole length of the timeSlot array but have the for loop finish at its end*/
-/* write a if statement so that if i is less than the value of the currentTime then the variablt timeColor will have a value of past */
+/* write a if statement so that if i is less than the value of the currentTime then the variable timeColor will have a value of past 
+(define the variable timeColor as equal to an empty string)*/
         if (i < currentTimePeriod) {
-            let timeColor = "past";
+            timeColor = "past";
         }
 /*write a else if statement so if i is equal only to the value of currentTimePeriod, the variablt timeColor will have a value of present*/
         else if (i === currentTimePeriod) {
-            let timeColor = "present";
+            timeColor = "present";
         }
         /*write a else if statement so if i is greater than the value of currentTimePeriod, the variable timeColor will have a value of future*/
         else if(i > currentTimePeriod) {
-            let timeColor = "future";
+            timeColor = "future";
         }
 /*Create a new `<div>` for each row and its  content  as the for loop runs*/
         let row = $("<div>");
@@ -59,11 +60,14 @@ then create a variable that is equal to the current hour so we can compare it ag
         let label = $("<label>");
   /*Add a class to the label to define its size using bootstrap */      
         label.addClass ("col-2 col-sm-1 time-block hour");
-
-        label.text(time[i]);
+/* attach the  text value of timeSlot (i) to the label*/
+        label.text(timeSlot[i]);
+/* append the class of label to each row*/
         row.append(label);
+ /*Create a new `<textarea>`  and its  content  as the for loop runs*/
         let textArea = $("<textarea>");
-        textArea.attr("class", "col-8 col-sm-10 description " t timeColor);
+/*add a set of classes to text area giving size */
+        textArea.addClass ( "col-8 col-sm-10 description " + timeColor);
         textArea.text(localStorage.getItem("btn" + i));
         row.append(textArea);
         let button = $("<button>");
