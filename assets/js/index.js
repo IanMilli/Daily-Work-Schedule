3ee3392daa6afc44c7ccdaa6b35856b85e9613ca
Ian M <ianmillichamp.2@gmail.com>
2023-01-23 12:48:21 +0000

    have discovered some bugs that are causing incorrect colours to display in incorrect places

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ let currentTimePeriod = moment().hour();;
 let timeColor = "";
 /*create required arrays here */
 
-let timeSlot = [07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,20,]
+let timeSlot = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,]
 
 console.log(currentTimePeriod);
 console.log(timeSlot);
@@ -20,11 +20,11 @@ the above tested and works but creates a static time display - i want a dynamic 
 for every second on the clock so will use the set interval system*/
 
 setInterval(
-    function () {
-        currentDayTimeText.text(moment().format("dddd, MMMM Do YYYY, H:mm:ss a"));
-        currentDayTime.append(currentDayTimeText);
+        function () {
+                currentDayTimeText.text(moment().format("dddd, MMMM Do YYYY, H:mm:ss a"));
+                currentDayTime.append(currentDayTimeText);
 
-    }, 1000);
+        }, 1000);
 
 /* next i wan t a function that will run as soon as the window loads to display visually whether the time rows are current (light green)
 past (red) or future(dark green)*/
@@ -34,62 +34,93 @@ window.onload = displayCurrentTime();
 
 function displayCurrentTime() {
 
-/*use a for loop to iterate through an array we will create below the global variable section.
-then create a variable that is equal to the current hour so we can compare it against the array to generate the correct display color*/
-    for (i = 0; i <= timeSlot.length - 1; i++) {
-/*first iterate along the whole length of the timeSlot array (-1 so it doesn't print unnecessary row)but have the for loop finish at its end*/
-/* write a if statement so that if i is less than the value of the currentTime then the variable timeColor will have a value of past 
-(define the variable timeColor as equal to an empty string)*/
-        if (timeSlot[i] < currentTimePeriod) {
-            timeColor = ".past";
-        }
-/*write a else if statement so if i is equal only to the value of currentTimePeriod, the variable timeColor will have a value of present*/
-        else if (i === currentTimePeriod) {
-            timeColor = ".present";
-        }
-        /*write a else if statement so if i is greater than the value of currentTimePeriod, the variable timeColor will have a value of future*/
-        else if(i > currentTimePeriod) {
-            timeColor = ".future";
-        }
-/*Create a new `<div>` for each row and its  content  as the for loop runs*/
-        let row = $("<div>");
-/*Add a class of row to the div  */
-        row.addClass("row");
-/*append the class of row to the container variable*/
-        container.append(row);
-/*Create a new label for each row and equal it to its container as the for loop runs */
+        /*use a for loop to iterate through an array we will create below the global variable section.
+        then create a variable that is equal to the current hour so we can compare it against the array to generate the correct display color*/
+        for (i = 0; i <= timeSlot.length - 1; i++) {
+                /*first iterate along the whole length of the timeSlot array (-1 so it doesn't print unnecessary row)but have the for loop finish at its end*/
+
+                /*Create a new `<div>` for each row and its  content  as the for loop runs*/
+                let row = $("<div>");
+                /*Add a class of row to the div  */
+                row.addClass("row");
+                /*append the class of row to the container variable*/
+                container.append(row);
+                /*Create a new label for each row and equal it to its container as the for loop runs */
 
 
-        let label = $("<label>");
-  /*Add a class to the label to define its size using bootstrap */  
-        label.addClass ("col-2 col-sm-1 time-block hour");
-/* attach the  text value of timeSlot (i) to the label*/
-        label.text(timeSlot[i]);
-/* append the value of the variable label to each variable row*/
-        row.append(label);
- /*Create a new `<textarea>`  and its  content  as the for loop runs*/
+                let label = $("<label>");
+                /*Add a class to the label to define its size using bootstrap */
+                label.addClass("col-2 col-sm-1 time-block hour");
+                /* attach the  text value of timeSlot (i) to the label*/
+                label.text(timeSlot[i]);
+                /* append the value of the variable label to each variable row*/
+                row.append(label);
+                /*Create a new `<textarea>`  and its  content  as the for loop runs*/
 
 
-        let textArea = $("<textarea>");
-/*add a set of classes to text area giving size and connect with css identifier*/
-        textArea.addClass ( "col-8 col-sm-10 description" + timeColor);
-/*attach the text value from local storage using a new function named getItem that works with the class of btn and the value of i at that point in the for loop */
-        textArea.text(localStorage.getItem("btn" + i));
-/*append the text area variable to the row variable value*/        
-        row.append(textArea);
-/* create a variable button and equal it to a button div to create a button at the end of each row */
+                let textArea = $("<textarea>");
+                /*add a set of classes to text area giving size and connect with css identifier*/
+                textArea.addClass("col-8 col-sm-10 description" + timeColor);
+                /*attach the text value from local storage using a new function named getItem that works with the class of btn and the value of i at that point in the for loop */
+                textArea.text(localStorage.getItem("btn" + i));
+                /*append the text area variable to the row variable value*/
+                /* write a if statement so that if i is less than the value of the currentTime then the variable timeColor will have a value of past 
+       (define the variable timeColor as equal to an empty string)*/
+                if (timeSlot[i] < currentTimePeriod) {
+                        textArea.addClass("past");
+                        textArea.removeClass("present");
+                        textArea.removeClass("future");
+                }
+                /*write a else if statement so if i is equal only to the value of currentTimePeriod, the variable timeColor will have a value of present*/
+                else if (i === currentTimePeriod) {
+                        textArea.addClass("present");
+                        textArea.removeClass("past");
+                        textArea.removeClass("future");
+                }
+                /*write a else if statement so if i is greater than the value of currentTimePeriod, the variable timeColor will have a value of future*/
+                else if (i > currentTimePeriod) {
+                        textArea.addClass("future");
+                        textArea.removeClass("present");
+                        textArea.removeClass("past");
+                }
 
 
-        let button = $("<button>");
-/* attach classes to the variable button to give size and css styling - use .attr rather than .addClass as some of the classes are viewed by jquery as attributes*/
-        button.attr("class", "col-2 col-sm-1 saveBtn fas fa-save");
-/* add an id to the button variable of btn plus the current value of i as the loop operates (id is an attribute so use .attr to add it) */
-        button.attr("id", "btn" + i);
- /*append the value of the variable to row */       
-        row.append(button);
-    };
+
+                row.append(textArea);
+                /* create a variable button and equal it to a button div to create a button at the end of each row */
+
+
+
+
+
+
+                let button = $("<button>");
+                /* attach classes to the variable button to give size and css styling - use .attr rather than .addClass as some of the classes are viewed by jquery as attributes*/
+                button.attr("class", "col-2 col-sm-1 saveBtn fas fa-save");
+                /* add an id to the button variable of btn plus the current value of i as the loop operates (id is an attribute so use .attr to add it) */
+                button.attr("id", "btn" + i);
+                /*append the value of the variable to row */
+                row.append(button);
+        };
+
+        /*the above for loop will create the day planner by forming a label with a time from the array in it , a text area for user input and a button
+        to allow the user to input there entry into local storage*/
+
+
+$(".saveBtn").on("click", function () {
+        let Input = $(textArea).siblings(".description").val();
+        console.log(description);
+        let time = $(textArea).parent().attr("id");
+        console.log(time);
+        localStorage.setItem(time, description);
+});
 }
-/*the above for loop will create the day planner by forming a label with a time from the array in it , a text area for user input and a button
-to allow the user to input there entry into local storage*/
-
-
+$("9 .description").val(localStorage.getItem(9));
+$("#10 .description").val(localStorage.getItem(10));
+$("#11 .description").val(localStorage.getItem(11));
+$("#12 .description").val(localStorage.getItem(12));
+$("#13 .description").val(localStorage.getItem(13));
+$("#14 .description").val(localStorage.getItem(14));
+$("#15 .description").val(localStorage.getItem(15));
+$("#16 .description").val(localStorage.getItem(16));
+$("#17 .description").val(localStorage.getItem(17));
